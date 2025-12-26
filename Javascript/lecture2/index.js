89c80e9b5b3442ca7a2636b16caa99418bf56f1f
Divyanshu-chauhan000 <divyanshuchauhan15072003@gmail.com>
2025-12-26 22:36:59 +0530

    day1

---
+++ Javascript/lecture2/index.js
@@ -0,0 +1,38 @@
+// var age = 21 // redeclare possible ---> confusion create ho skta ha 
+// if(true){
+//     console.log(age)
+// }
+//var age = 4    //undefined aayga agr niche declare krunga to
+
+
+// let a = 10;
+// console.log(a)
+
+// a=20; 
+// console.log(a) // redefinition is possible ----> acchha ha 
+
+
+// let a = 20;  redeclare possible nhi ha ---> achha ha 
+
+
+// const a = 28;
+// console.log(a)
+// a = 32;    redefinition not possible 
+// const a = 32; redeclare not possible ----> achha ha 
+
+
+let num = 20.23;
+console.log(num)
+
+let marks 
+console.log(marks)// undefined ---> ek variable k andr koi value define hi nahi ki 
+
+let num2 = null;
+console.log(num2) // null ---> ek variable k andr null value insert krlo
+
+let num3 = 644646844355453466876435153545468787654531658498764545544668;
+console.log(num3)
+
+
+let name = "Divyanshu"
+console.log(typeof(name))
---
+++ Javascript/lecture3/index.js
@@ -0,0 +1,23 @@
+// Arithmatic Operator
+//1. Binary -----> two operands  +,-,*,/,%,**
+//2. Unary -------> single operand ++, --
+
+// let a = 10;
+// let b= 20;
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a**2)
+
+// console.log(a++)
+// console.log(a--)
+// console.log(++a)
+// console.log(--a)
+
+//Assignment opertor --> =,+=,-=,/=,*=,%=
+
+// let a=2;
+// a+=2;
+// console.log(a) // output = 4
+
+
+//comparision operators 
